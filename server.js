3401139e1206a78a410a8ffad9b65d6a6e1415d9
Smack <Smack@Macintosh.local>
2018-02-08 17:18:19 -0500

    fixed PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 var app = express();
 
 
