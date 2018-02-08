3fefe7d0c7fa57113ba1b64e268101cfe38c3423
Smack <Smack@Macintosh.local>
2018-02-08 10:59:19 -0500

    working through bugs, trouble with userScores

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,49 @@ var friendData = require("../data/friends");
 
 module.exports = function (app) {
 
-app.get("/api/friends", (req, res) => {
+    app.get("/api/friends", (req, res) => {
         res.json(friendData);
     });
 
-app.post("/api/friends", function (req, res) {
-    console.log(friendData);
-})
+    app.post("/api/friends", function (req, res) {
+        // user input object
+        var userInput = req.body;
+        console.log(userInput);
+        // user input scores
+        var userScore = userInput.scores;
+        console.log(userScore);
+        // find match
+        var matchName = '';
+        var matchPhoto = '';
+        var totalDifference = 1000
 
-}
+        // looking at all friends in list
+        for (var i = 0; i < friendData.length; i++) {
+            // calculate differences for each question
+            var diff = 0;
+            for (var j = 0; j < userScore.length; j++) {
+                diff += Math.abs(friendData[i].scores[j] - userScore[j]);
+            }
+
+            if (diff < totalDifference) {
+                totalDifference = diff;
+                matchName = friends[i].name;
+                matchPhoto = friends[i].url;
+            }
+        }
+
+        friendData.push(userInput);
+
+        res.json({
+            status: 'OK',
+            matchName: matchName,
+            matchPhoto: matchPhoto
+        });
+
+
+
+
+
+    });
+
+};
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(bodyParser.text());
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
